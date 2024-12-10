df39a4721f34c450bbf287623631d99e276c8c45
johan guillen <sioupes34@gmail.com>
2024-12-10 14:35:13 +0100

    ex 8 tp javascript

--- ex8.js
+++ ex8.js
@@ -4,7 +4,7 @@ let randomNumber = Math.round(Math.random() * 100)
 let resultat ="perdu"
 console.log("random number = " + randomNumber)
 for (let i = 0; i < 5; i++) {
-    const num = prompt("combien ? ")
+    const num = parseInt(prompt("combien ? "))
     if (num < randomNumber) {
         console.log("c'est plus")
     }else if(num>randomNumber){
