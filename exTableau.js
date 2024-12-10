fdb1a074cd64f9489890bc63a891244212b35e15
johan guillen <sioupes34@gmail.com>
2024-12-10 17:44:37 +0100

    ex tableau tp javascript

--- exTableau.js
+++ exTableau.js
@@ -1,1 +1,26 @@
 let produits =[]
+
+const add =(produit) => produits.push(produit)
+
+const remove =(produit) => {
+    produits = produits.filter((p) => p !== produit)
+    return produits
+}
+
+const exists =(produit) => {
+
+
+    return (produits.find((p) => p===produit) !== undefined)
+}
+
+
+console.log(add("produit1"))
+console.log(add("produit2"))
+console.log(add("produit3"))
+
+console.log(exists("produit1"))
+
+console.log(remove("produit1"))
+
+
+console.log(exists("produit1"))
